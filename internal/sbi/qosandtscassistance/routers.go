501ba480bbb79dffe48ce2cf63605e6adb358276
SymbolWu <symbolwu.cs10@nycu.edu.tw>
2025-02-23 21:36:55 +0800

    add: qosandtscassistance service init

---
+++ internal/sbi/qosandtscassistance/routers.go
@@ -0,0 +1,107 @@
+package QoSandTSCAssistance
+
+import (
+	"net/http"
+	"strings"
+
+	logger_util "github.com/free5gc/util/logger"
+	"github.com/gin-gonic/gin"
+	"github.com/yashihh/tsctsf/internal/logger"
+)
+
+// Route is the information for every URI.
+type Route struct {
+	// Name is the name of this Route.
+	Name string
+	// Method is the string for the HTTP method. ex) GET, POST etc..
+	Method string
+	// Pattern is the pattern of the URI.
+	Pattern string
+	// HandlerFunc is the handler function of this route.
+	HandlerFunc gin.HandlerFunc
+}
+
+// Routes is the list of the generated Route.
+type Routes []Route
+
+// NewRouter returns a new router.
+func NewRouter() *gin.Engine {
+	router := logger_util.NewGinWithLogrus(logger.GinLog)
+	AddService(router)
+	return router
+}
+
+func AddService(engine *gin.Engine) *gin.RouterGroup {
+	group := engine.Group("/ntsctsf-time-sync/v1")
+
+	for _, route := range routes {
+		switch route.Method {
+		case "GET":
+			group.GET(route.Pattern, route.HandlerFunc)
+		case "POST":
+			group.POST(route.Pattern, route.HandlerFunc)
+		case "PUT":
+			group.PUT(route.Pattern, route.HandlerFunc)
+		case "DELETE":
+			group.DELETE(route.Pattern, route.HandlerFunc)
+		case "PATCH":
+			group.PATCH(route.Pattern, route.HandlerFunc)
+		}
+	}
+	return group
+}
+
+// Index is the index handler.
+func Index(c *gin.Context) {
+	c.String(http.StatusOK, "Hello World!")
+}
+
+var routes = Routes{
+	{
+		"Index",
+		"GET",
+		"/",
+		Index,
+	},
+
+	{
+		"PostTSCAppSessions",
+		strings.ToUpper("Post"),
+		"/tsc-app-sessions",
+		PostTSCAppSessions,
+	},
+	{
+		"GetTSCAppSession",
+		strings.ToUpper("Get"),
+		"/tsc-app-sessions/:appSessionId",
+		GetTSCAppSession,
+	},
+
+	{
+		"ModAppSession",
+		strings.ToUpper("Patch"),
+		"/tsc-app-sessions/:appSessionId",
+		ModAppSession,
+	},
+
+	{
+		"DeleteTSCAppSession",
+		strings.ToUpper("Delete"),
+		"/tsc-app-sessions/:appSessionId/delete",
+		DeleteTSCAppSession,
+	},
+
+	{
+		"putEventsSubsc",
+		strings.ToUpper("Put"),
+		"/tsc-app-sessions/:appSessionId/events-subscription",
+		putEventsSubsc,
+	},
+
+	{
+		"DeleteEventsSubsc",
+		strings.ToUpper("Get"),
+		"/tsc-app-sessions/:appSessionId/events-subscription",
+		DeleteEventsSubsc,
+	},
+}
